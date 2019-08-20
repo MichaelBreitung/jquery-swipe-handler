0fb366962f1bf58e4e50db5073b743c43fbcea38
mibreit-photo <Michael.Breitung@mibreit-photo.com>
2019-08-20 20:39:24 +0200

    minor: fixed function doc

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ from "./swipeCreator";
 
 /**
  * 
- * @param {css id or class name} target 
- * @param {array with containing selection of SWIPE_LEFT, SWIPE_RIGHT, SWIPE_UP, SWIPE_DOWN} handlers 
- * @param {(direction: string) => void} callback 
+ * @param target css id or class name  
+ * @param handlers array with containing selection of SWIPE_LEFT, SWIPE_RIGHT, SWIPE_UP, SWIPE_DOWN
+ * @param callback (direction: string) => void
  */
 export function handleSwipe(target, handlers, callback) {
   new SwipeCreator(target, handlers, callback);
