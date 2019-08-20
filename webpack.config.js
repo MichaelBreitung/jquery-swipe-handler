b15393628811af2285bd97397acb11a4b65c9d2e
mibreit-photo <Michael.Breitung@mibreit-photo.com>
2019-08-20 19:20:08 +0200

    Initial, empty revision

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = [{
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'jquerySwipeHandler.js',
+    library: 'jquery-swipe-handler',
+    libraryTarget: 'var'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['babel-preset-stage-0'].map(require.resolve)
+        }
+      }
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: false
+    })
+  ],
+  externals: {
+    jquery: 'jQuery'
+  }
+}, ];
